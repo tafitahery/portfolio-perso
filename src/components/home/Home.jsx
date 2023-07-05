84221aeaa7cb8b6b61f4060fd4b3814fda2c4970
Tafita Hery <tafitahery@gmail.com>
2023-07-05 12:28:20 +0300

    Home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,9 +4,14 @@ export default function Home() {
   return (
     <div className="home" id="home">
       <div className="wrapper">
-        <h1 className="title">FRONTEND DEVELOPPER</h1>
-        <p className="desc">Desc</p>
-        <div className="image">Photo</div>
+        <h1 className="title">Hi, I'm Tafita HERY</h1>
+        <p className="desc">
+          A Frontend focused Web Developer building the Frontend of Websites and
+          Web Applications that leads to the success of the overall product
+        </p>
+        <a href="#works" className="button">
+          works
+        </a>
       </div>
     </div>
   );
