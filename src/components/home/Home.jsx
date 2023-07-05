f869f532ed7beef793954f2bc470d7367eaef708
Tafita Hery <tafitahery@gmail.com>
2023-07-05 09:31:53 +0300

    Home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,11 @@ import './home.scss';
 export default function Home() {
   return (
     <div className="home" id="home">
-      Home
+      <div className="wrapper">
+        <h1 className="title">FRONTEND DEVELOPPER</h1>
+        <p className="desc">Desc</p>
+        <div className="image">Photo</div>
+      </div>
     </div>
   );
 }
