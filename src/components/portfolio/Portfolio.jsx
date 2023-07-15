b96d052e16973a6c10069722d069727e25983ace
Tafita Hery <tafitahery@gmail.com>
2023-07-15 16:25:07 +0300

    Portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,8 +12,36 @@ export default function Portfolio() {
           </p>
         </div>
         <div className="content">
-          <div className="left">left</div>
-          <div className="right">right</div>
+          <div className="left">
+            <h2>Get to know me!</h2>
+            <p>
+              I'm a Frontend Web Developer building the Front-end of Websites
+              and Web Applications that leads to the success of the overall
+              product. Check out some of my work in the Projects section. I also
+              like sharing content related to the stuff that I have learned over
+              the years in Web Development so it can help other people of the
+              Dev Community. Feel free to Connect or Follow me on my Linkedin
+              where I post useful content related to Web Development and
+              Programming I'm open to Job opportunities where I can contribute,
+              learn and grow. If you have a good opportunity that matches my
+              skills and experience then don't hesitate to contact me.
+            </p>
+            <a href="#contact">Contact</a>
+          </div>
+          <div className="right">
+            <h2>My Skills</h2>
+            <div className="skills">
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+              <span>React</span>
+              <span>SASS</span>
+              <span>GIT</span>
+              <span>Github</span>
+              <span>Responsive design</span>
+              <span>Terminal</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
