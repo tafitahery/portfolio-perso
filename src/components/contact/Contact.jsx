84c88435fb765d4e524f6d99725738ce51cd4c00
Tafita Hery <tafitahery@gmail.com>
2023-07-16 10:19:44 +0300

    Contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,42 @@ import './contact.scss';
 export default function Contact() {
   return (
     <div className="contact" id="contact">
-      Contact
+      <div className="wrapper">
+        <h1>CONTACT</h1>
+        <p>
+          Feel free to Contact me by submitting the form below and I will get
+          back to you as soon as possible
+        </p>
+        <form>
+          <div className="inputs">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter Your Name"
+            />
+          </div>
+          <div className="inputs">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter Your Email"
+            />
+          </div>
+          <div className="inputs">
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Enter Your Message"
+            ></textarea>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
